95dacd0a6e757cd76219cf2ffa3a2f289f4fab78
Michael McFadden <jimi1999uk@gmail.com>
2019-07-21 18:06:45 +0000

    Rock Paper Scissors exercise

---
+++ RB101/lesson_2/rps.rb
@@ -0,0 +1,46 @@
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
+def display_results(choice, computer_choice)
+  if win?(choice, computer_choice)
+    prompt("You won!")
+  elsif win?(computer_choice, choice)
+    prompt("You lost. Computer is the winner.")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?("y")
+end
+
+prompt("Thanks for playing. Goodbye!")
