a9b199aa2cf0ad543b3b9369ab48507d8b71c84d
Michael McFadden <jimi1999uk@gmail.com>
2019-07-25 21:17:36 +0000

    Rock Paper Scissors Lizard Spock game plus bonus

--- RB101/lesson_2/rps.rb
+++ RB101/lesson_2/rps.rb
@@ -1,32 +1,68 @@
-def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
-end
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
-def display_results(choice, computer_choice)
-  if win?(choice, computer_choice)
-    prompt("You won!")
-  elsif win?(computer_choice, choice)
-    prompt("You lost. Computer is the winner.")
-  else
-    prompt("It's a tie!")
-  end
-end
+player_move_beats = {'rock'=> ['lizard', 'scissors'], 'lizard' => ['spock', 'paper'],
+                   'spock' => ['scissors', 'rock'], 'scissors' => ['paper', 'lizard'],
+                   'paper' => ['rock', 'spock']}
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+human_count = 0
+computer_count = 0
+
+choice = ''
+computer_choice = VALID_CHOICES.sample
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def change_letter_to_word(user_word)
+  if VALID_CHOICES.include?(user_word)
+    user_word
+  elsif user_word == 'r'|| user_word == 'p'|| user_word == 's' || user_word == 'l'
+    if user_word == 'r'
+      'rock'
+    elsif user_word == 'p'
+      'paper'
+    elsif user_word == 'l'
+      'lizard'
+    elsif user_word == 's'
+      puts "Two 's' choices avaliable. Enter 'sp' to choose spock or 'sc' for scissors: "
+      input = gets.chomp
+        if input == 'sp'
+          'spock'
+        elsif input == 'sc'
+          'scissors'
+        else
+          change_letter_to_word(user_word)
+        end
+    end 
+  end
+end
+
+def display_results(human_move, computer_move)
+  player_move_beats = {'rock'=> ['lizard', 'scissors'], 'lizard' => ['spock', 'paper'], 
+                   'spock' => ['scissors', 'rock'], 'scissors' => ['paper', 'lizard'],
+                  'paper' => ['rock', 'spock'] }
+
+  if player_move_beats[human_move].include?(computer_move)
+    puts "Human wins! #{human_move} beats #{computer_move}."
+  elsif human_move == computer_move
+    puts "It's a tie. You both said #{human_move}"
+  else
+    puts "You LOST! The computer move of #{computer_move} beats #{human_move}."
+  end
+end
+
+
+prompt("First to 10 points wins. 2 pts for a win, 1 for tie.")
 loop do
-  choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    prompt("Enter: #{VALID_CHOICES.join(', ')} or the first letter of your choice.")
     choice = Kernel.gets().chomp()
 
     if VALID_CHOICES.include?(choice)
+      break
+    elsif ['r', 'p', 's', 'l'].include?(choice)
+      choice = change_letter_to_word(choice)
       break
     else
       prompt("That's not a valid choice.")
@@ -38,7 +74,25 @@ loop do
 
   display_results(choice, computer_choice)
 
-  prompt("Do you want to play again?")
+  if player_move_beats[choice].include?(computer_choice)
+    human_count += 2
+  elsif choice == computer_choice
+    human_count += 1
+    computer_count += 1
+  else
+    computer_count += 2
+  end
+  if human_count >= 10
+   puts "You are the GRAND WINNER with 5 wins!!!! Well done!"
+    break
+  elsif computer_count >= 10
+    puts "The machine wins. The AIcomputer the GRAND WINNER soz :-("
+  break
+  end
+
+puts "Score human: #{human_count}; Computer: #{computer_count}"
+
+ prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?("y")
 end
